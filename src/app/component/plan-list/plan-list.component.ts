2ad4dd0d403a79dbe01f5e943ce15ff8f4db00df
Sumant Chhunchha <mr.chhunchha@gmail.com>
2017-04-16 03:20:10 -0700

    Hosted site

--- src/app/component/plan-list/plan-list.component.ts
+++ src/app/component/plan-list/plan-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbDropdownConfig } from "@ng-bootstrap/ng-bootstrap";
+import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ai-plan-list',
--- src/app/component/secondary-header/secondary-header.component.ts
+++ src/app/component/secondary-header/secondary-header.component.ts
@@ -10,7 +10,7 @@ export class SecondaryHeaderComponent implements OnInit {
   public levels = [
     { name: 'Top Level', value: 0},
     { name: '1st Level', value: 1},
-    { name: '2snd Level', value: 2}
+    { name: '2nd Level', value: 2}
   ];
 
   public selectedLevel = this.levels[0];
