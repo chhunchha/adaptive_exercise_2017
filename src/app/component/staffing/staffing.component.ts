5f2f268ee4baebd8df68cb1909a1f2199db11233
Sumant Chhunchha <mr.chhunchha@gmail.com>
2017-04-16 14:22:03 -0700

    typo fix

--- src/app/component/staffing/staffing.component.ts
+++ src/app/component/staffing/staffing.component.ts
@@ -18,7 +18,7 @@ export class StaffingComponent implements OnInit {
   public staffingData = [
     ['' , 'Q1', 'Q2', 'Q3', 'Q4', 'FY16'],
     ['HC', 23, 26, 25, 29, 29 ],
-    ['NEW', 4, 7, 2, 6, 19 ],
+    ['New', 4, 7, 2, 6, 19 ],
     ['Terminations', 3, 5, 5, 2, 15 ]
   ];
 
